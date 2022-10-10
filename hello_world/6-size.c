d15f9525d276d2109a66665385394962b8f42a46
maria-m-rivera <5842@holbertonstudents.com>
2022-10-10 07:46:25 -0700

    Task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <studio.h>
+
+/**
+ * Description:
+ *
+ * Return: 0
+ */
+int main()
+{
+	int intType;
+	float floatType;
+	long int longintType;
+	long long longlongType;
+	char charType,
+
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of long int: %zu bytes\n", sizeof(longintType));
+	printf("Size of long long: %zu bytes\n", sizeof(longlongType));
+	printf("Size of char: %zu bytes\n", sizeof(charType));
+	
+	return 0;
+
+}
