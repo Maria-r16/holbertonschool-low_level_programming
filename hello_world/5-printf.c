53f018285ec1cf6dbd8d411bc014878074ed9791
maria-m-rivera <5842@holbertonstudents.com>
2022-10-10 06:48:28 -0700

    Task 5 -Hello print

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/**                                                            
- * main - prints "with proper grammar, but the outcome is a piece of art,\n"                                                         
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,\n"
  *
- * Description: print line of text followed by a new line, returning 0 as a result and should be compiled without warning. 
+ * Description: print line of text followed by a new line, returning 0 as a result and should be compiled without warning.
  *
- * Return: 0                                                                                                                          
- */                                                                                                                                   
-int main(void)                                                                                                                    
-{	
-	printf("with proper grammar, but the outcome is a piece of art,\n");                                                         
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}  
+}
