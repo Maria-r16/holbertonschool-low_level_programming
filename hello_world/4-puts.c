d0d5469682558e76d55ed6af492f58704df56080
maria-m-rivera <5842@holbertonstudents.com>
2022-10-10 05:29:16 -0700

    Prove

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #!/bin/bash
-gcc 
-}
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+#include <stdio.h>
+
+
+
+int main()
+
+{
+
+	    puts("Programming is like building a multilingual puzzle.\n");
+
+	        return 0;
+
 }
