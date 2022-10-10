44188379b5a51d9a026524cfd0f7a9de02ee1f17
maria-m-rivera <5842@holbertonstudents.com>
2022-10-10 09:08:46 -0700

    Task 6 -ImprTypeVarious

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-	
+
 	return (0);
 
 }
