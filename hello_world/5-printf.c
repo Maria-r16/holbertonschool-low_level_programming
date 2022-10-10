bddde155672ec973e7994cfaa785ab301e571c6a
maria-m-rivera <5842@holbertonstudents.com>
2022-10-10 06:32:12 -0700

    Task 5 -Hello print

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,13 +4,10 @@
  *
  *main - prints "with proper grammar, but the outcome is a piece of art,\n"                                                         
  *      
- *
  *Return: 0                                                                                                                          
  *
- *     
  */                                                                                                                                   
-
-int main(void)                                                                                                                        
+int main(void)                                                                                                                    
 {	
 	printf("with proper grammar, but the outcome is a piece of art,\n");                                                         
 	return (0);
