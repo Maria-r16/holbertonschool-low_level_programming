fe447ff4e7094fdc84dcc17c3468d88bf0490bd0
maria-m-rivera <5842@holbertonstudents.com>
2022-10-12 10:52:15 -0700

    Task 0 -Negative-positive

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,14 +3,13 @@
 #include <stdio.h>
 
 /*
- * Description: 
+ * Description: ssign value to the variable n, and determine based on it, if it is a negative or positive number or equal to its value
  *
- * main: 
+ * main - printf : output with newline
  *
  */
 
-int main (void)
-{
+int main (void) {
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
