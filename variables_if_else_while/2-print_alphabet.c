b9c8dea7ebdb39fe231d9ec62c2cd819ccd6f26d
maria-m-rivera <5842@holbertonstudents.com>
2022-10-12 21:04:20 -0700

    Task 2 -Putchar-Alpha

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ *main - main function
+ *Description: lowercase alphabetic sequence
+ *Return: 0
+ */
+
+int main ()
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+	{
+	putchar (alpha);
+	}
+	return (0)
+}
