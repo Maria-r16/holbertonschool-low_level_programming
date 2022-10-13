fce34ea4ad977fc82c6633176cb208a99a5b4430
maria-m-rivera <5842@holbertonstudents.com>
2022-10-13 10:07:22 -0700

    Task 5 -Numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -6,14 +6,14 @@
  * Return: 0
  */
 
-int main()
+int main(void)
 {
 	char num;
 
-	for (num = 0 ; num <= 9 ; num++)
+	for (num = '0' ; num <= '9' ; num++)
 	{
 	putchar (num);
-	if (num == 9)
+	if (num == '9')
 	{
 	putchar ('\n');
 	}
