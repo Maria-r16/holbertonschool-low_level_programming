90c0cbcdea07a20b76717f3302e5f7e92dbd0443
maria-m-rivera <5842@holbertonstudents.com>
2022-10-13 09:54:55 -0700

    Task 5 -Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description:
+ * Return: 0
+ */
+
+int main()
+{
+	char num;
+
+	for (num = '0' ; <= num '9' ; num++)
+	{
+	putchar (alpha);
+	if (num == '9')
+	{
+	putchar ('\n');
+	}
+	}
+	return (0);
+}
