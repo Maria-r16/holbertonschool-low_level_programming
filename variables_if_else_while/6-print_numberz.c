0b95fc03dab37bb6b3ffd7b4b4e7b1883afcc6fa
maria-m-rivera <5842@holbertonstudents.com>
2022-10-13 10:29:29 -0700

    Task 6 -Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *Description:
+ *Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0 ;  num <= 9 ; num++)
+	{
+	putchar (num+'0');
+	if (num == 9)
+	{
+	putchar ('\n');
+	}
+	}
+	return (0);
+}
