ac0c4610d059ac5c81ed104d3c18c307e8b9fb79
maria-m-rivera <5842@holbertonstudents.com>
2022-10-14 13:49:02 -0700

    Task 9 -Tabletimes

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -22,8 +22,8 @@ void times_table(void)
 		if (ret <= 9)
 			_putchar(' ');
 		else
-			_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+			_putchar((ret / 10) + '0');
+			_putchar((ret % 10) + '0');
 	}
 	_putchar('\n');
 	}
