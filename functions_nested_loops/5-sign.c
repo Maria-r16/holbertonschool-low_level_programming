e8c40f633544c977c598f2c8448620a9c44cf234
maria-m-rivera <5842@holbertonstudents.com>
2022-10-14 08:45:20 -0700

    Task 5 -Check-1+1=0

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,19 +11,19 @@ int print_sing(int n)
 {
 	if (n > 0)
 	{
-	_putchar(n);
+	_putchar(43);
 	return (1);
 	}
 
 	else if (n < 0)
 	{
-	_putchar(n);
+	_putchar(45);
 	return (-1);
 	}
 
 	else (n == 0)
 	{
-	_putchar(n);
+	_putchar(48);
 	return (0);
 	}
 }
