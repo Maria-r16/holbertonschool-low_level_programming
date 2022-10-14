0a95ea8b89bfc9bc22ba0845d67ae64030954c84
maria-m-rivera <5842@holbertonstudents.com>
2022-10-14 12:41:58 -0700

    Task 8 -DayJackBauer

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * jack_bauer - will print the general time of day jack bauer.
+ * Description:the functionality is performed by dictating the ten hour, hour unit, minutes and seconds
+ * Return: Hours, minutes and seconds
+ */
+
+void jack_bauer(void)
+{
+	int decenah, undh, min, seg
+
+	for (decenah = 0; decenah <= 2; decenah++)
+	{
+	for (undh =0; undh <= 9; undh++)
+	{
+		if ((decenah <= 1 && undh <= 9) || (decenah <= 2 && undh <= 3)) 
+	
+	{
+	for (min = 0; min <= 5; min++)
+	{
+	for (seg = 0; seg <=9; seg++)
+	{
+	
+		_putchar(decenah + '0');
+		_putchar(undh + '0');
+		_putchar(58);
+		_putchar(min + '0');
+		_putchar(seg + '0');
+		_putchar('\n');
+	}
+	}
+	}
+	}
+	}	
+
+
+}
