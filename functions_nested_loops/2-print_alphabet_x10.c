69ee2dbd7f1cc05d8f978189e616cc1268c34e00
maria-m-rivera <5842@holbertonstudents.com>
2022-10-13 23:11:39 -0700

    Task 2 -Alphabetx10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - alphabet printing teen using a new main function.
+ *Description: alphabet by teen times
+ *Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+for (int i = 0 ; i = 9 ; i++)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z' ; alpha++)
+	{
+	_putchar(alpha);
+	if (alpha == 'z')
+	{
+	_putchar('\n');
+	}
+	}
+}
+}
