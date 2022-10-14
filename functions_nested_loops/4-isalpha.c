9e6c4c77b257e59e841fbe4db8de99495389e667
maria-m-rivera <5842@holbertonstudents.com>
2022-10-14 08:05:01 -0700

    Task 4 -Check-Low-and_Upper

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check lowercase and uppercase character.
+ * @c - C - return one
+ * Description: where the letter c and C will have a return value: one.
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122 || C >= 97 && C <= 122)
+	{
+	return (1);
+	}
+	return (0);
+}
