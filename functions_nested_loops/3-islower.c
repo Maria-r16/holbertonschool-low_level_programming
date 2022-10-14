3d9aaeec6829c6b1d12448c7c027c5bf413d7879
maria-m-rivera <5842@holbertonstudents.com>
2022-10-14 07:21:04 -0700

    Task 3 -CheckLowecase

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *int_islower - check the lowercase character.
- *Description: check the lowercase, where the letter c will have a return value: one.
- *Return c: 1
- *Return: 0
- */
+*_islower - check the lowercase character.
+*Description: check the lowercase, where the letter c will have a return value: one.
+*c: return one
+*Return: 0
+*/
 
 int _islower(int c)
 {
