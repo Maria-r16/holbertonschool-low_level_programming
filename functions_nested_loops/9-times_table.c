ff58771420fb03498de9fa797fd44d9e586f2ab7
maria-m-rivera <5842@holbertonstudents.com>
2022-10-14 13:47:55 -0700

    Task 9 -Tabletimes

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * time_tablet - prints times table
+ * Return: - 
+ */
+
+void times_table(void)
+{
+	int n, mult, ret;
+
+	for (n = 0 ; n <= 9; n++)
+	{
+		_putchar('0');
+
+	for (mult = 1; mult <= 9; mult++)
+	{
+		_putchar(',');
+		_putchar(' ');
+		ret = n * mult;
+
+		if (ret <= 9)
+			_putchar(' ');
+		else
+			_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
