ddb205deeba57b8ae300e21abe4e8e2cd52736ea
maria-m-rivera <5842@holbertonstudents.com>
2022-10-14 08:36:51 -0700

    TAsk 5 prove

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sing -
+ * @n:
+ * Description:
+ * Return: 
+ */
+
+int print_sing(int n)
+{
+	if (n == 0)
+	{
+	_putchar(n);
+	return (0);
+	}
+
+	else if (n > 0)
+	{
+	_putchar(n);
+	return (1);
+	}
+
+	else (n < 0)
+	{
+	_putchar(n);
+	return (-1)
+	}
+}
