80daaa5391877c3374c37961b852c8e71aa64da3
maria-m-rivera <5842@holbertonstudents.com>
2022-10-19 01:24:47 -0700

    Task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,2 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * reset_to_98
+ * main - check the code
+ * @*n: pointer
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;	
+}
+ 
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
