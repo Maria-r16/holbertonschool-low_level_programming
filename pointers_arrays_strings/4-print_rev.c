d2bb9378e40b3cc37b2331566a1674fdeac7c47a
maria-m-rivera <5842@holbertonstudents.com>
2022-10-19 03:07:59 -0700

    Revers

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -2,12 +2,24 @@
 #include <stdio.h>
 
 /**
- * print_rev -
- * @s:
+ * print_rev - print a string the revers
+ * @s: pointer
  * Return: 0
  */
 
 void print_rev(char *s)
 {
-	printf("%s\n", strrev ( str ));	
+	int len = 0, i;
+
+	while (s[i++])
+	{
+		len++;
+	}
+
+	for (i = len - 1 ; i >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
 }
