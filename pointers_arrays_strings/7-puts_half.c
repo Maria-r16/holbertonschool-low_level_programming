f2751b0588bdeefc7bb2213abcc84f28f3b1403d
maria-m-rivera <5842@holbertonstudents.com>
2022-10-19 03:40:33 -0700

    Half-of-String

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half -
+ * @str:
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int a = 0, len = 0, n;
+
+	while (str[a++])
+	{
+		len++;
+	}
+
+	if ((len % 2) == 0)
+	{
+		n = len / 2;
+	}
+
+	else
+	{
+		n = (len +1) / 2;
+	}
+
+	for (a = n ; a < len ; a++)
+	{
+		putchar(str[a]);
+	}
+
+	putchar('\n');
+}
