a1c53897341870855272adc9bf3a8776e0ebe197
maria-m-rivera <5842@holbertonstudents.com>
2022-10-20 16:59:44 -0700

    CopyPunters

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strcpy - copies the string pointed to by src, including the terminating null byte (\0), into the buffer pointed to by (dest).
+ * _strcpy - copies the string pointed to by src.
  * @dest:buffer pointed
  * @src:string to copy
  * Return: 0
