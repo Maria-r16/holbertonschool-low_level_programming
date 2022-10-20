18f03214937795bee1e8cde3cce6de2a3136f417
maria-m-rivera <5842@holbertonstudents.com>
2022-10-20 16:56:14 -0700

    CopyPunters

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i])
+	while (*(str + i))
 	{
-		dest[i] = src[i];
+		*(dest[i]) = *(src[i]);
 		i++;
 	}
+	*(dest[i]) = '\0';
 
 	return (dest);
 }
