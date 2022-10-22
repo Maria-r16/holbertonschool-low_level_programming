3ad1428877b6ba7335d737576e4acb9cfd7d8911
maria-m-rivera <5842@holbertonstudents.com>
2022-10-22 09:26:19 -0700

    Correction_Betty

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * _strchr - locates a character in a string.
  * @s: the string to check.
  * @c: character to check.
- * Return: Returns a pointer to the first occurrence of the character c in the string s, or NULL.
+ * Return: Returns a poiniter to the first occurrence of the character @c.
  */
 
 char *_strchr(char *s, char c)
