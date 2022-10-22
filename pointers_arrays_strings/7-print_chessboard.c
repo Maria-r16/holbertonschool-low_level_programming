076deb1c74b1c0c0eafede466fc433d4dad20bfb
maria-m-rivera <5842@holbertonstudents.com>
2022-10-22 10:18:44 -0700

    Correction_Cod

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_chessboard - prints the chessboard.
@@ -17,11 +16,11 @@ void print_chessboard(char (*a)[8])
 		{
 			if (j == 7)
 			{
-				_putchar(a[i][j]);
-				_putchar('\n');
+				putchar(a[i][j]);
+				putchar('\n');
 			}
 			else 
-				_putchar(a[i][j]);
+				putchar(a[i][j]);
 		}
 	}
 }
