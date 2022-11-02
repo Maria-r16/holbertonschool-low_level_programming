a881c412e90c17805018d57d0a7ae729864eb4c5
maria-m-rivera <5842@holbertonstudents.com>
2022-11-02 09:11:11 -0700

    .

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,5 +1,5 @@
-#include "main.h"
-
+#include <main.h>
+#include <stdio.h>
 /**
  * _print_rev_recursion - prints a string in reverse
  *  @s: the string to print
@@ -10,6 +10,6 @@ void _print_rev_recursion(char *s)
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		putchar(*s);
 	}
 }
