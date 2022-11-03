6146121909b76aef430634d5a3a1ffd7c6dde206
Maria-r16 <majori.200116@gmail.com>
2022-11-03 18:24:46 -0500

    prove

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,1 +1,33 @@
-#include 
+#include "main.h"
+#include <stdio.h>
+
+/** 
+* main -
+* 
+* @argc:
+*
+* @argv:
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+  int result;
+  
+  if (argc != 2)
+  {
+    printf("Error\n");
+    return (1);
+  }
+  
+  int num1 = argv[1];
+  int num2 = argv[2];
+
+  result = num1 * num2;
+    printf(result);
+
+  return 0;
+}
+
+
