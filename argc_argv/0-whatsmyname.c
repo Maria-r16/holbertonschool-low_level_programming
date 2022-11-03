4d2e4c3513f938d9e6b1e8a988fa883eb85c6c2f
maria-m-rivera <5842@holbertonstudents.com>
2022-11-03 08:52:05 -0700

    Prove

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - type the name followed by a new line.
+ *
+ * @argc: number of arguments
+ *
+ * @argv: pointer to an array of character strings
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv)
+{
+	argc = 0;
+
+	printf("%s/n", argv[argc]);
+	return (0);
+}
---
+++ argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -
+ *
+ * @argc:
+ *
+ * @srgv:
+ *
+ * Return:
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("%d\n", argc);
+	for(i = 1 ; i < argc ; i++)
+		{
+			printf("%s", argv[i]);
+		}
+	return 0;
+}
