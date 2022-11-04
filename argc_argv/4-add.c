666bbf51ea2ba25a39cb192466f8f20af8d77e03
Maria-r16 <majori.200116@gmail.com>
2022-11-04 00:17:35 -0500

    Code

---
+++ argc_argv/4-add.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - add positive numbers.
+* 
+* @argv: argument list the function main.
+* 
+* @argc: number of arguments
+* 
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+int suma = 0;
+int i = 0;
+int j = 0;
+
+if (argc == 1)
+{
+printf("0\n");
+return (1);
+}
+
+while (i < argc)
+{
+while (j != '\0')
+{
+if (argv[i][j] >= '0' && argv[i][j] <= '9')
+{
+  continue;
+}
+else 
+{
+printf("Error\n");
+}    
+j++;
+}
+suma += atoi(argv[i]); 
+i++;
+}
+
+printf("%d\n", suma);
+
+return (0);
+}
