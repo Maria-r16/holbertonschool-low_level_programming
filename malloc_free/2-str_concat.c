52e92d90d56f25d012b71c190430d4add528dfdb
Maria-r16 <majori.200116@gmail.com>
2022-11-07 00:38:01 -0500

    Code 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,31 +15,45 @@
 char *str_concat(char *s1, char *s2)
 {
 char *s;
-int size1, size2, i, j;
+int size1 = 0;
+int size2 = 0;
+int i = 0;
+int j = 0;
 
-if (s1 == 0)
+if (s1 == NULL)
 s1 = "";
 
-if (s2 == 0)
+if (s2 == NULL)
 s2 = "";
 
-for (size1 = 0; s1[size1] != '\0'; size1++);
-
-for (size2 = 0; s2[size2] != '\0'; size2++);
-
-size2++;
-s = malloc(size1 * sizeof(*s1) + size2 * sizeof(*s2));
-
-if (s == 0)
-return (NULL);
-
-for (i = 0, size2 = 0; i < size1 + size2; i++)
+while (s1[size1] != '\0')
 {
-if (i < size1)
-s[i] = s1[i];
-
-else
-s[i] = s2[j++];
+size1++;
 }
+
+while (s2[size2] != '\0')
+{
+size2++;
+} 
+
+s = (char *)malloc((size1 + size2 + 1) * sizeof(char));
+
+if (s == NULL)
+{
+  return (NULL);
+}
+
+while (s1[i] != '\0')
+{
+s[i] = s1[i];
+i++;
+}
+ 
+while (s2[j] != '\0')
+{
+s[j] = s2[j];
+j++;
+}
+
 return (s);
 }
