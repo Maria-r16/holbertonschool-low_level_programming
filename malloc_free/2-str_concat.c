2e0afde620f65c799ddf666a04745b98c1e12db0
Maria-r16 <majori.200116@gmail.com>
2022-11-07 00:44:23 -0500

    Code 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -4,21 +4,15 @@
 
 /**
 * str_concat -  concatenates two strings
-*
 * @s1: string 1
-*
 * @s2: string 2
-*
 * Return: NULL
 */
-
 char *str_concat(char *s1, char *s2)
 {
 char *s;
-int size1 = 0;
-int size2 = 0;
-int i = 0;
-int j = 0;
+int size1 = 0, size2 = 0;
+int i = 0, j = 0;
 
 if (s1 == NULL)
 s1 = "";
