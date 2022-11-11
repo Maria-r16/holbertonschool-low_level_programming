7541e551aaf1eda752ffaf4ed3ba35c720a5dfc9
juanwehehe <juan.wehehe@gmail.com>
2022-11-11 17:28:07 -0500

    Free-Dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - function that releases allocated memory for a dog_t
+ * @d: Address of to free
+ * Return: no value
+ */
+
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
