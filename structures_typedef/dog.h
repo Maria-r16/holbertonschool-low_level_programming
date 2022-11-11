4fb38c658baf5f9378cc9090f996de11d68a6696
juanwehehe <juan.wehehe@gmail.com>
2022-11-11 16:52:57 -0500

    New_dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -21,6 +21,7 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 /** previous prototypes to use in point 4:*/
+int _putchar(char c);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 #endif
