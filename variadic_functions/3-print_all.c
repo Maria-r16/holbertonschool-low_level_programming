4b8c3283829c9d1c0f3a80a6672d6dda0b1ad2ec
juanwehehe <juan.wehehe@gmail.com>
2022-11-11 23:32:16 -0500

    Dir-types

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,13 +1,46 @@
 #include "variadic_functions.h"
 
 /**
- * print_all -
- * 
- * @format:
- * 
- * Return:
+ * print_all - function that prints arguments of different types
+ *
+ * @format: list of types of arguments
+ *
+ * Return: nothing
 */
 void print_all(const char * const format, ...)
 {
+types data[] = {
+{"c", character},
+{"i", integer},
+{"f", floatelement},
+{"s", string},
+{NULL, NULL}
+};
 
+int i = 0, j = 0;
+char *sep;
+
+va_list list;
+
+sep = "";
+va_start(list, format);
+
+while (format && format[i] != '\0')
+{
+while (data[j].ch != NULL)
+{
+if (format[i] == data[j].ch[0])
+{
+printf("%s", sep);
+data[j].func(list);
+sep = ", ";
+}
+
+j++;
+}
+
+i++;
+}
+va_end(list);
+printf("\n");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define VARIADICT_FUNCTIONS
 #include <stdio.h>
 #include <stdarg.h>
+#include "type_functions.h"
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
