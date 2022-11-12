44b2ed7e71e6d3fa254625e5e308d904bee19eec
juanwehehe <juan.wehehe@gmail.com>
2022-11-11 21:24:34 -0500

    code

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,4 +1,39 @@
-#include "stdlib.h"
 #include "3-calc.h"
-#include <stdio.h>
 
+/**
+* main - program that performs simple operations (Calculator)
+*
+* @argc: number of arguments
+*
+* @argv: argument vector+
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+int cant1, cant2;
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+cant1 = atoi(argv[1]);
+cant2 = atoi(argv[3]);
+
+if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+
+if ((argv[2] == '/' && cant2 == 0) || (argv[2] == '%' && cant2 == 0))
+{
+printf("Error\n");
+exit(100);
+}
+	
+printf("%d\n", get_op_func(argv[2])(cant1, cant2));
+return (0);
+}
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
 #include "3-calc.h"
 
 /**
   * op_add - sum of two variables
   * @a: the first num type int
   * @b: the second num type int
-  *
   * Return: result the sum
   */
 int op_add(int a, int b)
@@ -13,19 +11,46 @@ int op_add(int a, int b)
 return (a + b);
 }
 
+/**
+ * op_sub - the subtraction of two numbers
+ * @a: The first number
+ * @b: The second number
+ * Return: result
+ */
 int op_sub(int a, int b)
 {
 return (a - b);
 }
 
 /**
- * op_mod - the division of two numbers
+ * op_mul - the multiplication of two numbers
  * @a: The first number
  * @b: The second number
- *
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - the division of two numbers
+ * @a: The first number
+ * @b: The second number
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - the modulation of two numbers
+ * @a: The first number
+ * @b: The second number
  * Return: result
  */
 int op_mod(int a, int b)
 {
-	return (a % b);
+return (a % b);
 }
