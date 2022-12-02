1034d6155c46da31f4bbc6749e6f0cc559178053
Maria-r16 <majori.200116@gmail.com>
2022-12-02 16:10:58 -0500

    Betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,20 +8,24 @@
 
 size_t print_list(const list_t *h)
 {
+
 size_t len = 0;
 int i = 0;
 const list_t *c = h;
 
 while (c)
 {
+
 if (c->str == NULL)
 {
 printf("[0] (nil)\n");
 }
+
 else
 {
 printf("[%d] %s\n", c->len, c->str);
 }
+
 c = c->next;
 len++;
 i++;
