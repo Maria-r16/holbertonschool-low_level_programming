e7c8c9880b883740b952ca2843a6f77393042587
Maria-r16 <majori.200116@gmail.com>
2022-12-03 14:16:59 -0500

    Betty

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -6,7 +6,6 @@
 * @str: The string
 * Return: The address of the new element, or NULL if it failed
 */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *newnode;
@@ -14,12 +13,11 @@ list_t *last;
 int coun = 0;
 
 if (head == NULL || str == NULL)
+{
 return (NULL);
-
+}
 last = *head;
-
 newnode = malloc(sizeof(list_t));
-	
 if (newnode == NULL)
 {
 return (NULL);
@@ -34,16 +32,13 @@ while (str[coun] != '\0')
 {
 coun++;
 }
-
 newnode->len = coun;
 newnode->next = NULL;
-
 if (*head == NULL)
 {
 *head = newnode;
 return (newnode);
 }
-
 while (last->next != NULL)
 {
 last = last->next;
