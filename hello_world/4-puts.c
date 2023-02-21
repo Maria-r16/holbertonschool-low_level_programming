ef174c784934ada47dd8f17ab52bbcc68ca5d0b5
Maria-r16 <majori.200116@gmail.com>
2023-02-21 14:59:32 -0500

    change

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - prints "Programming is like building a multilingual puzzle"
-*
-* Return: 0
+ * main - print the phrase "Programming is like building a multilingual puzzle
+ * 
+ * Description: 
+ * 
+ * Retrun: 0
 */
 
-int main(void)
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle\n");
+    return (0);
 }
