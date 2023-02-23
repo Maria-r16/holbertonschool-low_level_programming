2649cfd9b4aa9f1f9b9e315640c4d98285d35f8a
Maria-r16 <majori.200116@gmail.com>
2023-02-22 19:42:45 -0500

    change else for if

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-	else (n > 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else (n < 0)
+	if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
