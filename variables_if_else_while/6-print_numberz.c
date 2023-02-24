2c48733b4d0158ca0f1c3ede17ff48ce12196f53
Maria-r16 <majori.200116@gmail.com>
2023-02-24 11:03:02 -0500

    prints all single digit numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - function main
+* Description: prints all single digit numbers
+* Return: 0
+*/
+
+int main(void)
+{
+	int number;
+
+	for (number = '0' ; number <= '9' ; number++)
+	{
+		putchar(number);
+		if (number == '9')
+		{
+			putchar('\n');
+		}
+	}
+}
