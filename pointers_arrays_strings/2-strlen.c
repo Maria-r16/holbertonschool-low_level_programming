923f20cfc52fafc312a859c7758398fa6beb5207
Maria-r16 <majori.200116@gmail.com>
2023-03-17 15:12:31 -0500

    ✒️ Delete main's

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -13,19 +13,3 @@ int i = 0;
 	++i;
 	return (i);
 }
-
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-char *str;
-int len;
-
-str = "My first strlen!";
-len = _strlen(str);
-printf("%d\n", len);
-return (0);
-}
