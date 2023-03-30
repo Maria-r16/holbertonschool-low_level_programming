fd9ac5d1b72dc7fab8120dc387b3c276144f2974
Maria-r16 <majori.200116@gmail.com>
2023-03-30 14:43:08 -0500

    🚀 Task Preprocessor

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef HOLBERTON_SCHOOL
+#define HOLBERTON_SCHOOL
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
+#endif
---
+++ preprocessor/check/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the file name of the program
+  *        followed by a new line.
+  *
+  * Return: 0
+  */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
